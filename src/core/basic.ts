e0f11d9862ef08a61c4bf5cb00253e8a131c5153
xiaoMan <1195566313@qq.com>
2024-03-26 18:16:19 +0800

    basic.ts

---
+++ src/core/basic.ts
@@ -0,0 +1,38 @@
+import type { Env } from "../types/common";
+/**
+ * 
+ * @returns true if the current environment is a browser false if the current environment is not a browser
+ * @returns 是否是浏览器
+ */
+export const isBrowser = ()=> {
+    return typeof window !== 'undefined' && typeof window.document !== 'undefined';
+}
+/**
+ * 
+ * @returns true if the current environment is Node false if the current environment is not Node
+ * @returns 是否是Node环境
+ */
+export const isNode = ()=> {
+    return typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+}
+/**
+ * @returns  true if the current environment is WebWorker false if the current environment is not WebWorker
+ * @returns 是否是WebWorker
+ */
+export const isWebWorker = ()=> {
+    return typeof self !== 'undefined' && typeof self.postMessage !== 'undefined' && typeof importScripts !== 'undefined';
+}
+
+
+export const env = ():Env => {
+    if (isBrowser()) {
+        return 'browser';
+    }
+    if (isNode()) {
+        return 'node';
+    }
+    if (isWebWorker()) {
+        return 'webworker';
+    }
+    return 'unknown';
+}
---
+++ src/types/common.ts
@@ -0,0 +1,4 @@
+
+export type Env = 'browser' | 'node' | 'webworker' | 'unknown' ;
+export type Mobile = 'ios' | 'android' | 'web' | 'miniapp';
+export type Desktop = 'macos' | 'windows' | 'linux' | 'electron';
