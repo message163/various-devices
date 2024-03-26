fc3c47a626c01a21a783b7b7f45b6fb0d76e5dba
xiaoMan <1195566313@qq.com>
2024-03-26 19:32:58 +0800

    node

--- example/index.ts
+++ example/index.ts
@@ -1,2 +1,4 @@
 import { getEnv } from '../src/core/basic'
+import { getPlatformNode } from '../src/core/node'
 console.log(getEnv())
+console.log(getPlatformNode())
--- src/core/node.ts
+++ src/core/node.ts
@@ -1,3 +1,13 @@
-export const getPlatformNode = () => {
-   
+import { getEnv } from "./basic"
+import type { Result } from '../types/common'
+export const getPlatformNode = (): Result => {
+    const node = getEnv()
+    if (node == 'node') {
+      return {
+        platform: node,
+        version: process.version,
+      }
+    } else {
+        throw new Error('不是Node环境 (is not Node environment)')
+    }
 }
